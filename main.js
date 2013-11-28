0b408f3806be433686cd820870576e76ac5d4b50
Gavrilov Miroslav <gavrilov.miroslav@gmail.com>
2013-11-27 23:07:43 -0800

    radionica #4
    sa sve domacim.

--- main.js
+++ main.js
@@ -4,8 +4,24 @@ window.onload = function() {
 		engine.config = {
 			debug : true,
 			assets : {
+				/* <drvo> */
+					drvo1 : { 
+						path : "assets/drvo.png", 
+						type : $.asset_types.IMAGE 
+					},
+					drvo_idle : { 
+						frames : [ "drvo1" ], 
+						type : $.asset_types.ANIMATION,
+						repeat : engine.constants.repeat_types.LOOP 
+					},
+					drvo : {
+						paths : { 
+							idle : "drvo_idle" 
+						},
+						type : $.asset_types.SPRITE
+					},
+				/* </drvo> */
 				/* <damir> */
-				
 					// slike i prva animacija
 					damir1 : { path : "assets/damir1.png", type : $.asset_types.IMAGE },				
 					damir2 : { path : "assets/damir2.png", type : $.asset_types.IMAGE },				
@@ -49,51 +65,85 @@ window.onload = function() {
 				drawing.context.fillRect(0, 0, drawing.canvas.width, drawing.canvas.height);
 			}
 
+			var treex = -100;
+			var treey = 0;
+
+			for(var i = 1; i < 40; i++) {
+				treey += Math.random() * 5;
+				engine.plugins.scene.stage.addChild({
+					name : "Drvo" + i,
+					asset : "drvo",
+					position : { 
+						x : Math.random() * engine.drawing.canvas.width, 
+						y : treey - 150 
+					}
+				});
+			}
+			treey += 40
+			for(var i = 1; i < 15; i++) {
+				treex += 100 + Math.random() * 5;
+				treey += Math.random() * 5;
+				engine.plugins.scene.stage.addChild({
+					name : "Drvo" + i,
+					asset : "drvo",
+					position : { 
+						x : treex, 
+						y : treey - 150 
+					}
+				});
+			}
+
+
 			engine.plugins.scene.stage.addChild({				
 				name : "Damir",
 				asset : "damir",
 
 				position : { 
-					x : Math.random() * engine.drawing.canvas.width,
-					y : Math.random() * engine.drawing.canvas.height
+					x : engine.drawing.canvas.width / 2,
+					y : engine.drawing.canvas.height / 2
 				},
 
 				tag : {
-					holding_mouse : true,
-					rotation_speed : 10 * (Math.random() - 0.5)
+					target : { 
+						x : engine.drawing.canvas.width / 2,
+						y : engine.drawing.canvas.height / 2
+					},
 				},
 				
-				onKey : function(self, event) {
-					if(event.key == 32 && event.state) {
-						self.tag.holding_mouse = !self.tag.holding_mouse;
-					} else if(event.key != 32) {
-						self.asset.path_at = "attack";
-					}
+				onKey : function(self, event) {	
+
 				},
 
 				onMouse : function(self, event) {
 					if(event.state) {
-						if(engine.plugins.scene.hitUnderMouse() === self) {
+						if(engine.plugins.scene.hitUnderMouse() !== null) {
 							self.scaleTo(1.1, 1.1);
+						} else {
+							self.tag.target.x = event.x;
+							self.tag.target.y = event.y;
 						}
 					} else self.scaleTo(1, 1);
 				}
-			}).addUpdateHook(function(self) {
-				if(self.tag.holding_mouse) {
-					self.moveTo(engine.plugins.input.mouse_position.x, engine.plugins.input.mouse_position.y);
+			})
+			.addUpdateHook(function(self) {
+				if(self.position.x == self.tag.target.x &&
+					self.position.y == self.tag.target.y) {
+					// stigli na odrediste i nista vise
+				} else {		
+					var a = self.position;
+					var b = self.tag.target;
+					// { x : 10, y : 12 } - { x : 9, y : 6 }
+					// posto ne mozemo da oduzimamo objekte,
+					// moramo oduzimati komponente x, y posebno
+					var dx = b.x - a.x;
+					var dy = b.y - a.y;
+					// vremenska delta izmedju dva frejma
+					var delta = engine.core.time.delta();
+					// mrdamo damira za deo puta koji moze da
+					// stane u delta-vremenu
+					self.moveBy(dx * delta, dy * delta);
 				}
-				self.rotateBy(self.tag.rotation_speed * engine.core.time.delta());
-			}).addRenderHook(function(self, drawing) {
-				drawing.context.save();
-				drawing.context.fillStyle = "#fff";
-				drawing.context.fillText(self.tag.holding_mouse ? "Mouse Held" : "Mouse Free", 100, 30);
-				drawing.context.restore();
-			}).addLateRenderHook(function(self, drawing) {
-				drawing.context.save();
-				drawing.context.fillStyle = "#fff";
-				drawing.context.fillText(self.tag.holding_mouse ? "Mouse Held" : "Mouse Free", 100, 30);
-				drawing.context.restore();
-			});
+			})
 		});
 	});
 }
